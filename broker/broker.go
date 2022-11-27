935eb9a34083cf2d2212247fa74f2c16677d1342
jesson3 <71626129+jesson3@users.noreply.github.com>
2022-11-27 05:59:30 +0000

    add transfer functions

--- broker/broker.go
+++ broker/broker.go
@@ -10,6 +10,7 @@ import (
 type Msg struct {
 	Id       int64
 	TopicLen int64
+	Topic    string
 	MsgType  int64
 	Len      int64
 	Payload  []byte
@@ -30,5 +31,47 @@ func BytesToMsg(reader io.Reader) Msg {
 	// topicLen
 	buff = bytes.NewBuffer(buf[8:16])
 	binary.Read(buff, binary.LittleEndian, &m.TopicLen)
+	// topic
+	msgLastIndex := 16 + m.TopicLen
+	m.Topic = string(buf[16:msgLastIndex])
+	// msgtype
+	buff = bytes.NewBuffer(buf[msgLastIndex : msgLastIndex+8])
+	binary.Read(buff, binary.LittleEndian, &m.MsgType)
+
+	buff = bytes.NewBuffer(buf[msgLastIndex+8 : msgLastIndex+16])
+	binary.Read(buff, binary.LittleEndian, &m.Len)
+
+	if m.Len <= 0 {
+		return m
+	}
+
+	m.Payload = buf[msgLastIndex+16:]
 	return m
+}
+
+func MsgToBytes(msg Msg) []byte{
+	msg.TopicLen = int64(len([]byte(msg.Topic)))
+	msg.Len = int64(len([]byte(msg.Payload)))
+
+	var data []byte
+	buf := bytes.NewBuffer([]byte{})
+	binary.Write(buf, binary.LittleEndian, msg.Id)
+	data = append(data, buf.Bytes()...)
+
+	buf = bytes.NewBuffer([]byte{})
+	binary.Write(buf, binary.LittleEndian, msg.TopicLen)
+	data = append(data, buf.Bytes()...)
+
+	data = append(data, []byte(msg.Topic)...)
+
+	buf = bytes.NewBuffer([]byte{})
+	binary.Write(buf, binary.LittleEndian, msg.MsgType)
+	data = append(data, buf.Bytes()...)
+
+	buf = bytes.NewBuffer([]byte{})
+	binary.Write(buf, binary.LittleEndian, msg.Len)
+	data = append(data, buf.Bytes()...)
+	data = append(data, []byte(msg.Payload)...)
+
+	return data
 }
